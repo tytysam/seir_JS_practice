b9890516b2bd15f0e415dfebf2a5efd2062a99d0
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-29 19:29:57 -0600

    verbal questions complete

---
+++ function_drills/solutions.js
@@ -0,0 +1,9 @@
+//What is the difference between a parameter and an argument?
+// --> a parameter is essentially a placeholder established when you declare a
+// --> function/expression, whereas an argument is the actual object/value
+// --> passed into the function.
+
+// Within a function, what is the difference between return and console.log?
+// --> while console.log will display the parameter that you're passing to the
+// --> log method in the console window. return will stop the function from executing
+// --> and return the specified value
