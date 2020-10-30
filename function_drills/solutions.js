e3bddf60f40246277652d128ef3649460a044f54
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-29 22:53:07 -0600

    final commit for function_drill problems

--- function_drills/solutions.js
+++ function_drills/solutions.js
@@ -16,49 +16,49 @@
 
 //TEST --> 'racecar'
 
-// const checkPalindrome = (string) => {
-//   //we could use a regular expression to remove any unwanted spaces/characters, but that might be beyond me tonight.
-//   let lowerCaseString = string.toLowerCase();
-//   //RESOURCE: https://medium.com/sonyamoisset/reverse-a-string-in-javascript-a18027b8e91c
-//   let reverseString = lowerCaseString.split("").reverse().join("");
-//   return reverseString === lowerCaseString;
-// };
+const checkPalindrome = (string) => {
+  //we could use a regular expression to remove any unwanted spaces/characters, but that might be beyond me tonight.
+  let lowerCaseString = string.toLowerCase();
+  //RESOURCE: https://medium.com/sonyamoisset/reverse-a-string-in-javascript-a18027b8e91c
+  let reverseString = lowerCaseString.split("").reverse().join("");
+  return reverseString === lowerCaseString;
+};
 
 // console.log(checkPalindrome("RaCeCaR"));
 
 // //Sum Array
-// const numbers = [1, 2, 3, 4, 5, 6];
+const numbers = [1, 2, 3, 4, 5, 6];
 
-// let newSum = 0;
-// const sumArray = (array) => {
-//   for (let i = 0; i < array.length; i++) {
-//     newSum += array[i];
-//   }
-//   return newSum;
-// };
+let newSum = 0;
+const sumArray = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    newSum += array[i];
+  }
+  return newSum;
+};
 
 // console.log(sumArray(numbers));
 
 //Prime Numbers
-// const checkPrime = (num) => {
-//   //You need to start i @ 2 because it is the first prime number.
-//   for (let i = 2; i < num; i++) {
-//     if (num % i === 0) return false;
-//   }
-//   return true;
-// };
+const checkPrime = (num) => {
+  //You need to start i @ 2 because it is the first prime number.
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
-// const printPrimes = (num) => {
-//   //Again, we need to start our array with a value of 2 since that is the first
-//   //prime number
-//   let arr = [2];
-//   for (let i = 3; i < num; i += 2) {
-//     if (checkPrime(i)) {
-//       arr.push(i);
-//     }
-//   }
-//   console.log(arr);
-// };
+const printPrimes = (num) => {
+  //Again, we need to start our array with a value of 2 since that is the first
+  //prime number
+  let arr = [2];
+  for (let i = 3; i < num; i += 2) {
+    if (checkPrime(i)) {
+      arr.push(i);
+    }
+  }
+  console.log(arr);
+};
 
 // console.log(checkPrime(7));
 
