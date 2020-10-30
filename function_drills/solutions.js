00d81e7509cdb3c33d0a4edff838a75e8caf1419
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-29 20:02:32 -0600

    palindrome completed

--- function_drills/solutions.js
+++ function_drills/solutions.js
@@ -7,3 +7,21 @@
 // --> while console.log will display the parameter that you're passing to the
 // --> log method in the console window. return will stop the function from executing
 // --> and return the specified value
+
+//Palindrome
+//WHAT STEPS DO WE NEED???
+// 1.) Reverse the word.
+// 2.) Check if the word is the same as the reverse
+// 3.) Return true or false
+
+//TEST --> 'racecar'
+
+const checkPalindrome = (string) => {
+  //we could use a regular expression to remove any unwanted spaces/characters, but that might be beyond me tonight.
+  let lowerCaseString = string.toLowerCase();
+  //RESOURCE: https://medium.com/sonyamoisset/reverse-a-string-in-javascript-a18027b8e91c
+  let reverseString = lowerCaseString.split("").reverse().join("");
+  return reverseString === lowerCaseString;
+};
+
+console.log(checkPalindrome("RaCeCaR"));
